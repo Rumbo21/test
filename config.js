3d85b2ce6e7757d82366a6a1d303e4c09e34910a
Rumbo21 <101298978+Rumbo21@users.noreply.github.com>
2023-06-03 04:55:08 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4bguGRa3TAbFlyeN9hMUnmuBVjyixl6w",
+  authDomain: "exampleproject-4936e.firebaseapp.com",
+  projectId: "exampleproject-4936e",
+  storageBucket: "exampleproject-4936e.appspot.com",
+  messagingSenderId: "7959950249",
+  appId: "1:7959950249:web:b65fc5539a806eb9390ab8",
+  measurementId: "G-D7J2NESJV8"
+};
