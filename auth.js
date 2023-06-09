c04c513ae30dffef45c77bbea9bf5b01119b9aad
Ameer Khan <101298978+Rumbo21@users.noreply.github.com>
2023-06-09 14:48:27 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,14 @@
 function signUp () {
- email = document.getElementById('email').value;
- password = document.getElementById('password').value;
- auth.createUserWithEmailAndPassword(email, password).then(cred => {
- alert(cred.user.email + ' has been registered')
+  email = document.getElementById('email').value;
+  password = document.getElementById('password').value;
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    alert(cred.user.email + ' has been registered')
  })
 }
 function login() {
- email = document.getElementById('email').value;
- password = document.getElementById('password').value;
- auth.signInWithEmailAndPassword(email, password).then(cred => {
- alert(cred.user.email + ' has logged in')
+  email = document.getElementById('email').value;
+  password = document.getElementById('password').value;
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    alert(cred.user.email + ' has logged in')
  })
 }
